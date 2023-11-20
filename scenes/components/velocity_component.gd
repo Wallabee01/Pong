acf569e1a0ce401ca457f372decc59af2fbbd94f
Grumn <phrozenfire01@gmail.com>
2023-11-20 12:18:23 -0800

    Day One
    Created main, ball, player one

---
+++ scenes/components/velocity_component.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@export var max_speed: int = 40
+@export var acceleration: float = 5.0
+
+var velocity = Vector2.ZERO
+
+
+func accelerate_in_direction(direction: Vector2):
+	var final_velocity = direction * max_speed
+	velocity = velocity.lerp(final_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+
+
+func decelerate():
+	accelerate_in_direction(Vector2.ZERO)
+
+
+func move(character_body: CharacterBody2D):
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+	#move_and_slide() returns the remaining velocity, but is internal to CharacterBody2D
+	#so if we want to capture the remaining velocity, we need to assign our VelocityComponent's 
+	#velocity to it, will only be different if the CharacterBody2D collides with something
+	velocity = character_body.velocity
---
+++ scenes/players/player_one.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var velocity_component = $VelocityComponent
+
+
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
+
+
+func get_movement_vector() -> Vector2:
+	var y_movement = Input.get_action_strength('player_one_move_down') - Input.get_action_strength('player_one_move_up')
+	
+	return Vector2(0, y_movement)
